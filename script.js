a8e862cd12b817de538468becf7bfdccb3fcd983
Alyssa Doerscher <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-08-28 14:59:17 -0500

    timer starts on game page

--- script.js
+++ script.js
@@ -131,17 +131,21 @@ $(document).ready(function(){
 		chooseNumbers();
 		populateNumbers();
 		targetNumber();
+		timer();
 	})
 
 	// main timer
 	var count = 60;
-	var timer = setInterval(function(){
-		if(count === 0) {
-			outcome();
-			clearInterval(timer);
-		}
-		$('#seconds').html(count--);
-	}, 1000)
+
+	function timer() {
+		setInterval(function(){
+			if(count === 0) {
+				outcome();
+				clearInterval(timer);
+			}
+			$('#seconds').html(count--);
+		}, 1000)
+	}
 
 	// populate math problem
 	var problemNumbers1 = [[8, 7, 6, 4, 3], [5, 3, 5, 2, 6], [1, 7, 5, 5, 1],[9, 7, 1, 8, 4], [3, 4, 9, 4, 7], [9, 6, 6, 8, 9], [5, 9, 5, 3, 1], [4, 8, 4, 6, 8], [4, 9, 8, 1, 7],[9, 9, 8, 2, 6], [4, 1, 9, 6, 7], [5, 6, 5, 9, 8],[9, 4, 5, 7, 2], [8, 9, 7, 7, 8], [8, 1, 1, 4, 3],[9, 8, 2, 7, 6], [7, 7, 1, 2, 8], [4, 7, 3, 7, 5],[1, 3, 2, 6, 5], [2, 3, 9, 4, 2], [4, 9, 7, 6, 2],[6, 9, 4, 1, 3], [3, 1, 9, 1, 1], [7, 6, 2, 4, 8],[8, 4, 5, 2, 4], [6, 9, 2, 4, 3], [4, 9, 8, 6, 8],[2, 4, 6, 1, 6], [3, 7, 5, 9, 2], [5, 3, 1, 8, 1],[8, 7, 5, 8, 4], [3, 7, 6, 3, 5], [2, 1, 2, 8, 5],[7, 8, 2, 1, 6], [8, 6, 1, 9, 5], [7, 4, 3, 8, 5],[6, 1, 5, 4, 7], [3, 3, 1, 8, 2], [1, 3, 9, 2, 6],[2, 3, 7, 1, 6], [5, 8, 9, 4, 3], [9, 2, 7, 3, 1],[9, 7, 8, 6, 3], [6, 3, 2, 5, 3], [8, 3, 1, 4, 7],[6, 2, 9, 8, 3], [7, 1, 4, 7, 9], [2, 3, 5, 9, 7],[1, 9, 4, 1, 6], [9, 8, 6, 3, 1]];
@@ -169,10 +173,11 @@ $(document).ready(function(){
 	}
 
 	function chooseNumbers() {
-		var index = Math.floor(Math.random()*9);
+		var index = Math.floor(Math.random()*50);
 
-		if (usedArray.length == 8) {
-			return;
+		if (usedArray.length == 50) {
+			usedArray = [];
+			chooseNumbers();
 		} else if(usedArray.indexOf(index) === -1) {
 			whichArray = index;
 			usedArray.push(index);
